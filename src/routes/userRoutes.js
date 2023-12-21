e0740357f4cabd1580a5844c5433a444e8d8a3e9
hardikshakya <hardikshakya1997@gmail.com>
2023-12-21 12:43:45 +0530

    feat(auth-middleware): added JWT middleware to protect routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const { registerUser, loginUser } = require('../controllers/userController');
+const verifyToken = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
@@ -9,5 +10,10 @@ router.post('/register', registerUser);
 
 // Login route
 router.post('/login', loginUser);
+
+// Protected route
+router.get('/protected', verifyToken, (req, res) => {
+  res.send({ message: 'This is a protected route.' });
+});
 
 module.exports = router;
